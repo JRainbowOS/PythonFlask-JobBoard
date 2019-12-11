d5328b74a76aaf78e7a96b31b997a29f661f067d
Jacob Rainbow <Jacob.Rainbow@os.uk>
2019-12-11 17:14:18 +0000

    fixed sql connection

--- jobs/app.py
+++ jobs/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 def open_connection():
     connection = getattr(g, '_connection', default=None)
-    if connection is None:
+    if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row 
     return connection
@@ -15,7 +15,7 @@ def open_connection():
 def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute_sql(sql, values)
-    if commit:
+    if commit == True:
         results = connection.commit()
     else:
         results = cursor.fetchone() if single else cursor.fetchall()
@@ -26,7 +26,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', default=None)
-    if connection:
+    if connection is not None:
         connection.close()
 
 @app.route('/')
